e231af59b6acf00755298914c29ec9dd278bbb76
黄毅 <yi.codeplayer@gmail.com>
2014-08-01 11:51:17 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,15 @@ ext_modules = [ Extension("credis.base", ["credis/base.c"])
 
 setup(
     name = 'credis',
-    version = '1.0.1',
+    version = '1.0.2',
     packages=['credis'],
     ext_modules = ext_modules,
+    author='huangyi',
+    author_email='yi.codeplayer@gmail.com',
+    url = 'https://github.com/yihuang/credis',
+    description = 'high performance redis client implemented with cython',
+    long_description=open('README.rst').read(),
+    install_requires = [
+        'hiredis >= 0.1',
+    ],
 )
