697b3166418ea6b963fd9c4d3551794916ed33ff
Cody Lee <cody.lee@datadoghq.com>
2020-08-11 11:15:11 -0500

    revert import and flip logic so py3 way is preferred

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-from distutils.core import setup
+try:
+    from distutils.core import setup
+except ImportError:
+    from setuptools import setup
+
 from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
