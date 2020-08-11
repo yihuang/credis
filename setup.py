72f238a89997658d7fd5fd2b49fc4b50d1c3bde6
Cody Lee <cody.lee@datadoghq.com>
2020-08-11 10:15:29 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("credis.base", ["credis/base.pyx"]),
-               Extension("credis.geventpool", ["credis/geventpool.pyx"]),
-               ]
-cmdclass = {'build_ext': build_ext}
+ext_modules = [
+    Extension("credis.base", ["credis/base.pyx"]),
+    Extension("credis.geventpool", ["credis/geventpool.pyx"]),
+]
+cmdclass = {"build_ext": build_ext}
 
 setup(
-    name='credis',
-    version='1.0.9',
-    packages=['credis'],
+    name="credis",
+    version="2.0.0",
+    packages=["credis"],
     ext_modules=ext_modules,
     cmdclass=cmdclass,
-    author='huangyi',
-    author_email='yi.codeplayer@gmail.com',
-    url='https://github.com/yihuang/credis',
-    description='high performance redis client implemented with cython',
-    long_description=open('README.rst').read(),
-    install_requires=[
-        'hiredis >= 0.1',
+    author="huangyi",
+    author_email="yi.codeplayer@gmail.com",
+    url="https://github.com/yihuang/credis",
+    description="high performance redis client implemented with cython",
+    long_description=open("README.rst").read(),
+    install_requires=["hiredis >= 1.1.0",],
+    platforms=[],
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Python :: 3",
+        "Python :: 3.8",
+        "Python :: 3.9",
+        "Cython",
+        "Python :: Implementation :: Cython",
     ],
 )
