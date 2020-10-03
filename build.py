a8b5c8f90a8a459c6b8d35fac4eef6fe031ff796
yihuang <yi.codeplayer@gmail.com>
2020-10-04 00:24:14 +0800

    temp

--- build.py
+++ build.py
@@ -1,19 +1,18 @@
 from distutils.extension import Extension
 
-try:
-    from Cython.Build import build_ext, cythonize
-except ImportError:
-    def build(_):
-        pass
-else:
-    def build(setup_kwargs):
-        """Build extension modules."""
-        setup_kwargs.update({
-            'ext_modules': cythonize(
+from Cython.Build import build_ext, cythonize
+
+
+def build(setup_kwargs):
+    """Build extension modules."""
+    setup_kwargs.update(
+        {
+            "ext_modules": cythonize(
                 [
                     Extension("credis.base", ["credis/base.pyx"]),
                     Extension("credis.geventpool", ["credis/geventpool.pyx"]),
                 ],
             ),
-            'cmdclass': {'build_ext': build_ext}
-        })
+            "cmdclass": {"build_ext": build_ext},
+        }
+    )
